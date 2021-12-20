916bba55fab9b87e04f27631cf759762b51da099
GOU NAITO <e215709@ie.u-ryukyu.ac.jp>
2021-12-20 13:22:01 +0900

    step3 done

--- src/Report5.java
+++ src/Report5.java
@@ -1,12 +1,18 @@
 class Report5{
     public static void main(String[] args){
+        /** 
         String str = null;
         try{
         System.out.println(str.length());
         }catch (NullPointerException e) {
-            //NullPointerExceptionが起きたときの処理
-            System.out.println("NullPointerException発生！");
-            System.out.println(e.getMessage());
-        }
+        //NullPointerExceptionが起きたときの処理
+        System.out.println("NullPointerException発生！");
+        System.out.println(e.getMessage()); 
+       
+        }*/
+        String str = "百二十三";
+        System.out.println(Integer.parseInt(str));
+        int value =  Integer.parseInt(str);
+
     }
 }
